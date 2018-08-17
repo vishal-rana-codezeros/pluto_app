4d1c001e18942aff5c2280348b3e4fd3ed122bce
vishal rana <vishal.rana@codezeros.com>
2018-08-17 14:18:54 +0530

    pluto app schema design

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+//Visit this for jshint -https://github.com/jshint/jshint/blob/4c2d091b7e50ce2681ee48a104578bb969c189ae/examples/.jshintrc#L79
+module.exports = function (grunt) {
+
+
+
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+          yamlToJson:{
+
+          }
+    });
+
+    // Tell Grunt what to do when we type "grunt" into the terminal
+    grunt.registerTask('default', [
+      
+    ]);
+};
---
+++ server.js
@@ -0,0 +1,39 @@
+console.log("");
+console.log("//************************* Chat Bot **************************//");
+console.log("");
+
+
+
+
+//Import Config
+const config = require('./lib/config');
+
+
+config.dbConfig(config.cfg, (err) => {
+    if (err) {
+        console.error(err, 'exiting the app.');
+        return;
+    }
+
+
+    // load external modules
+    const express = require("express");
+
+    // init express app
+    const app = express();
+
+    // set server home directory
+    app.locals.rootDir = __dirname;
+
+    // config express
+    config.expressConfig(app, config.cfg.environment);
+
+    // attach the routes to the app
+    require("./lib/route")(app);
+
+    // start server
+    app.listen(config.cfg.port, () => {
+        console.info(`Express server listening on ${config.cfg.port}, in ${config.cfg.TAG} mode`);
+    });
+
+});
