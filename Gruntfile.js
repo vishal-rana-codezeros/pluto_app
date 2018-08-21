755b8a43a26686088f956c274e313ce15c7a752c
vishal rana <vishal.rana@codezeros.com>
2018-08-21 16:33:20 +0530

    grunt file update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,3 @@
-//Visit this for jshint -https://github.com/jshint/jshint/blob/4c2d091b7e50ce2681ee48a104578bb969c189ae/examples/.jshintrc#L79
 module.exports = function (grunt) {
 
     grunt.initConfig({
